61a17d79608cecb844e409fb85bf7a75b53da649
Yasser <yaserqamhiah@gmail.com>
2024-09-10 00:24:03 +0300

    implement simple api gateway

--- inventory-service/src/main/java/com/microservice/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/microservice/inventory_service/InventoryServiceApplication.java
@@ -4,13 +4,12 @@ import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
-	@Autowired
-	@Lazy
-	private EurekaClient eurekaClient;
 
 
 	public static void main(String[] args) {
